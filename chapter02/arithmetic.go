873f629c7609d18786a8f67ac5ddb372a8cb8631
小川智裕 <tomo.ogawa18@gmail..com>
2022-09-06 21:18:41 +0900

    add: chapter2 arithmetic.go

---
+++ chapter02/arithmetic.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(1 + 2)
+	fmt.Println("abc" + "XYZ")
+	fmt.Println(3 - 2)
+	fmt.Println(2 * 3)
+	fmt.Println(5 / 2)
+	fmt.Println(-5 / 2)
+	fmt.Println(5 % 2)
+	fmt.Println(3 & 6)
+	fmt.Println(3 | 6)
+	fmt.Println(3 &^ 6)
+	fmt.Println(2 << 1)
+	fmt.Println(2 >> 1)
+}
