7fe18ba93110cff3ef5dba034e90298c801284fc
小川智裕 <tomo.create30@gmail.com>
2022-09-08 19:06:06 +0900

    add: chapter03 method_value.go

---
+++ chapter03/method_value.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type myType int
+
+func (value *myType) add(increment myType) myType {
+	*value += increment
+	return *value
+}
+
+func main() {
+	var i myType
+
+	fmt.Println(i)
+	fmt.Println(i.add(3))
+	mv := i.add
+	fmt.Println(mv(3))
+	fmt.Println(i)
+}
