a5d5c7245b9f04329ceb5686d2484ba948dd8c4b
小川智裕 <tomo.create30@gmail.com>
2022-09-08 16:25:05 +0900

    add: chapter03 variadic.go

---
+++ chapter03/variadic.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	holiday(1, "元旦", "成人の日")
+	holiday(2, "建国記念の日")
+	holiday(3, "春分の日")
+}
+
+func holiday(month int, days ...string) {
+	fmt.Printf("%d月の祝日は%d日あります。", month, len(days))
+	fmt.Println()
+	for _, day := range days {
+		fmt.Println(day)
+	}
+
+	fmt.Println()
+
+}
