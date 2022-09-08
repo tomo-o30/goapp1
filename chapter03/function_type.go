1018217f4910f41602b702d564d6512bdf526024
小川智裕 <tomo.create30@gmail.com>
2022-09-08 16:47:39 +0900

    add: chapter03 function_type.go

---
+++ chapter03/function_type.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var f func(int, int) int
+	f = func(a int, b int) int {
+		return a + b
+	}
+	fmt.Println(f(1, 2))
+
+	f = multiply
+	fmt.Println(f(1, 2))
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
