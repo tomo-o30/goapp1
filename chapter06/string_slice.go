308c1ae66acd087df27378b9e465f4cb06e223d7
tomo-o30 <tomo.ogawa18@gmail.com>
2022-09-11 13:14:57 +0900

    add: chapter06 string_slice.go

---
+++ chapter06/string_slice.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x string = "abcdef"[1:4]
+	var y string = "あいうえお"[3:9]
+	var z string = "あいうえお"[1:4]
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
