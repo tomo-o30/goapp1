9d68cabf4b9ad00a6b0e6818a4e6ff85068febd6
tomo-o30 <tomo.ogawa18@gmail.com>
2022-09-11 12:54:16 +0900

    add: chapter06 slice.go

---
+++ chapter06/slice.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := [5]string{"a", "b", "c", "d", "e"}
+
+	//スライス型の変数
+	var s1 []string
+
+	//配列全体をスライス
+	s1 = x[:]
+	fmt.Println(s1)
+
+	s2 := x[1:4]
+	fmt.Println(s2)
+
+	s3 := x[3:]
+	fmt.Println(s3)
+
+	s4 := x[:4]
+	fmt.Println(s4)
+}
