ca368b667737004737386f522876466a24f6c282
tomo-o30 <tomo.ogawa18@gmail.com>
2022-09-17 13:24:47 +0900

    add: chapter09 sleep.go

---
+++ chapter09/sleep.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println(time.Now())
+	time.Sleep(10 * time.Second)
+
+	fmt.Println(time.Now())
+	time.Sleep(1 * time.Minute)
+
+	fmt.Println(time.Now())
+}
